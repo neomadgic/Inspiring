b9fe2f19132c57061fe7856c87a41ceff14cd05c
Vu Dang <jublusion5@gmail.com>
2017-06-03 16:11:54 -0500

    Successfully downloaded the txt file

--- Inspiring/ViewController.swift
+++ Inspiring/ViewController.swift
@@ -14,6 +14,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
+        let url = URL(string: "http://dev.inspiringapps.com/Files/IAChallenge/30E02AAA-B947-4D4B-8FB6-9C57C43872A9/Apache.log")
+        do{
+            let urlContents = try String(contentsOf: url!, encoding: String.Encoding.utf8)
+            
+            print(urlContents)
+        }catch let error as NSError{
+            print(error)
+        }
+//        URLSession.shared.dataTask(with: url!, completionHandler: {
+//            (data, response, error) in
+//            if(error != nil){
+//                print("error")
+//            }else{
+//                do{
+//                    let urlContents = try String(contentsOf: url!, encoding: String.Encoding.utf8)
+//                    
+//                    print(urlContents)
+//                }catch let error as NSError{
+//                    print(error)
+//                }
+//            }
+//        }).resume()
     }
 
     override func didReceiveMemoryWarning() {
