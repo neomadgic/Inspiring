b8ef39277b54c19816bd6f89897948bf6fe38e6c
Vu Dang <jublusion5@gmail.com>
2017-06-03 16:27:05 -0500

    refractored downloadApacheLog

--- Inspiring/ViewController.swift
+++ Inspiring/ViewController.swift
@@ -10,39 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var apacheLog = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        let url = URL(string: "http://dev.inspiringapps.com/Files/IAChallenge/30E02AAA-B947-4D4B-8FB6-9C57C43872A9/Apache.log")
-        do{
-            let urlContents = try String(contentsOf: url!, encoding: String.Encoding.utf8)
-            
-            print(urlContents)
-        }catch let error as NSError{
-            print(error)
-        }
-//        URLSession.shared.dataTask(with: url!, completionHandler: {
-//            (data, response, error) in
-//            if(error != nil){
-//                print("error")
-//            }else{
-//                do{
-//                    let urlContents = try String(contentsOf: url!, encoding: String.Encoding.utf8)
-//                    
-//                    print(urlContents)
-//                }catch let error as NSError{
-//                    print(error)
-//                }
-//            }
-//        }).resume()
+        downloadApacheLog()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func downloadApacheLog() {
+        
+        DispatchQueue.global(qos: .background).async {
+            let url = URL(string: "http://dev.inspiringapps.com/Files/IAChallenge/30E02AAA-B947-4D4B-8FB6-9C57C43872A9/Apache.log")
+            do {
+                let urlContents = try String(contentsOf: url!, encoding: String.Encoding.utf8)
+                self.apacheLog = urlContents
+            } catch let error as NSError{
+                print(error)
+            }
+            
+            DispatchQueue.main.async {
+                print(self.apacheLog)
+            }
+        }
+    }
 }
 
