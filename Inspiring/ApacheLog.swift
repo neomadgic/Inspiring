dc23be23780a7954e2aeec234377baaaa915a3cc
Vu Dang <jublusion5@gmail.com>
2017-06-03 19:07:30 -0500

    Parsed the relevent data from apache log

---
+++ Inspiring/ApacheLog.swift
@@ -0,0 +1,15 @@
+//
+//  ApacheLog.swift
+//  Inspiring
+//
+//  Created by Vu Dang on 6/3/17.
+//  Copyright © 2017 Vu Dang. All rights reserved.
+//
+
+import Foundation
+
+struct ApacheLog {
+    
+    var user: String
+    var request: String
+}
--- Inspiring/ViewController.swift
+++ Inspiring/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var apacheLog = ""
+    var apacheLog = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,15 +29,28 @@ class ViewController: UIViewController {
             let url = URL(string: "http://dev.inspiringapps.com/Files/IAChallenge/30E02AAA-B947-4D4B-8FB6-9C57C43872A9/Apache.log")
             do {
                 let urlContents = try String(contentsOf: url!, encoding: String.Encoding.utf8)
-                self.apacheLog = urlContents
+                //print(urlContents.components(separatedBy: "\n")[80])
+                self.apacheLog = urlContents.components(separatedBy: "\n")
+                self.parseApacheLog(with: self.apacheLog[0])
             } catch let error as NSError{
                 print(error)
             }
             
             DispatchQueue.main.async {
-                print(self.apacheLog)
+                //print(self.apacheLog)
             }
         }
+    }
+    
+    func parseApacheLog(with: String) {
+        
+        var user = String()
+        var request = String()
+    
+        user = with.components(separatedBy: " ")[0]
+        request = "\(with.components(separatedBy: " ")[5]) \(with.components(separatedBy: " ")[6]) \(with.components(separatedBy: " ")[7])"
+        
+        print("\(user), \(request)")
     }
 }
 
