6e3a98a4cb7898fe71dcd0a970844d9c45e628bd
Vu Dang <jublusion5@gmail.com>
2017-06-06 18:14:08 -0500

    Successfully brute forced the solution. Next step is to optimize

--- Inspiring/ViewController.swift
+++ Inspiring/ViewController.swift
@@ -10,17 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var apacheDictionary = [String:Any]()
+    var testDictionary = [String:Int]()
+    var apacheDictionary = [String: [String: [String:Any]]]()
     var apacheLog = [ApacheLog]()
+    var letsCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        apacheDictionary["John"] = "Butt"
-        print(isPageEqual(newPage: "Butt", oldPage: apacheDictionary["John"] as! String?))
-        
-        
-        //downloadApacheLog()
+        downloadApacheLog()
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,6 +34,36 @@ class ViewController: UIViewController {
                 let urlContents = try String(contentsOf: url!, encoding: String.Encoding.utf8)
                 let apacheLogArray = urlContents.components(separatedBy: "\n")
                 
+                for x in 0...apacheLogArray.count - 2 {
+                    
+                    let user = apacheLogArray[x].components(separatedBy: " ")[0]
+                    let page = apacheLogArray[x].components(separatedBy: " ")[6]
+                    
+                    
+                    if let oldPage = self.apacheDictionary[user]?["currentStreak"]?["name"] as? String {
+                        if self.isPageEqual(newPage: page, oldPage: oldPage) {
+                            let oldCount = (self.apacheDictionary[user]?["currentStreak"]?["count"] as? Int)!
+                            self.apacheDictionary[user] = ["currentStreak" : ["name": page, "count": oldCount + 1]]
+                            if oldCount + 1 >= 3 {
+                                if let threePageCount = self.testDictionary["\(user) \(page) count"] {
+                                    self.testDictionary["\(user) \(page) count"] = threePageCount + 1
+                                } else {
+                                    self.testDictionary["\(user) \(page) count"] = 1
+                                    self.apacheLog.append(ApacheLog(user: user, page: page, count: 0))
+                                }
+                            }
+                            self.letsCount += 1
+                        } else {
+                            self.apacheDictionary[user] = ["currentStreak" : ["name": page, "count": 1]]
+                            self.letsCount += 1
+                        }
+                    } else {
+                        self.apacheDictionary[user] = ["currentStreak" : ["name": page, "count": 1]]
+                        self.letsCount += 1
+                    }
+                    
+                    
+                }
                 
                 
             } catch let error as NSError{
@@ -43,7 +71,9 @@ class ViewController: UIViewController {
             }
             
             DispatchQueue.main.async {
-                print(self.apacheLog)
+                print(self.apacheDictionary.count)
+                print(self.testDictionary.count)
+                print(self.apacheLog[0].printLog())
             }
         }
     }
